4ce0cd134e3fc43b24304f4a54683d185fbd9df4
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-11-17 07:14:51 +0700

    update example 5

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -148,7 +148,7 @@ final class Versions
   'sebastian/type' => '2.3.4@b8cd8a1c753c90bc1a0f5372170e3e489136f914',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.1@34a41e998c2183e22995f158c581e7b5e755ab9e',
-  'laravel/lumen' => 'dev-main@687baa0a83b63c115290b7b595265d315737812d',
+  'laravel/lumen' => 'dev-main@0b9f77fb0bc4f0f5ae98c6a37f8a68c897954434',
 );
 
     private function __construct()
